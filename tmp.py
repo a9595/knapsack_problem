4393a9f2a84e39ef3637b2cb56da34b46d4b2a25
Andrii Kovalchuk tieorange <tieorange@gmail.com>
2015-06-11 13:29:33 +0200

    finisher, but not optimized

--- tmp.py
+++ tmp.py
@@ -3,9 +3,9 @@ from itertools import compress, product
 __author__ = 'tieorange'
 
 # sizes = [3, 10, 10, 1, 6, 5, 5, 4, 2, 7, 9, 9, 8, 2, 1, 5, 4, 1, 9, 8, 2, 8, 7, 3, 10, 7]
-# values = [7, 7, 16, 11, 6, 3, 1, 15, 3, 8, 3, 13, 11, 15, 8, 13, 2, 12, 14, 1, 2, 18, 9, 8, 19, 5]
-sizes = [3, 10, 10, 1]
-vals = [7, 7, 16, 11]
+# vals = [7, 7, 16, 11, 6, 3, 1, 15, 3, 8, 3, 13, 11, 15, 8, 13, 2, 12, 14, 1, 2, 18, 9, 8, 19, 5]
+sizes = [3, 10, 10, 1, 6, 5, 5, 4, 2, 7, 9, 9, 8, 2]
+vals = [7, 7, 16, 11, 6, 3, 1, 15, 3, 8, 3, 13, 11, 15]
 LENGTH = len(sizes)
 CAPACITY = 40
 
@@ -26,23 +26,15 @@ CAPACITY = 40
 def get_char_vector(count):
     char_vector = list()
     for i in product([0, 1], repeat=count):
-        char_vector.append(i)
+        char_vector.append(list(i))
     return char_vector
 
 
 def calc_list_vector_price(vector_list):
+    prices_vector = list()
     for vector in vector_list:
-        calc_list_vector_price(vector, sizes, vals)
-
-
-def totalvalue(tmp_list):
-    sum_size = 0
-    sum_val = 0
-    for i, (size, value) in enumerate(tmp_list):
-        sum_val += value
-        sum_size += size
-    CAPACITY = 40
-    return sum_val if sum_size <= CAPACITY else (0, 0)
+        prices_vector.append(calc_list_vector_price(vector, sizes, vals))
+    return prices_vector
 
 
 def calc_vector_price(vector, sizes, values):
@@ -50,11 +42,35 @@ def calc_vector_price(vector, sizes, values):
     for idx, val in enumerate(vector):
         if val == 1:
             tmp_list.append((sizes[idx], values[idx]))
-    return totalvalue(tmp_list)
+    return get_val_sum(tmp_list)
 
 
-res = calc_vector_price([0, 0, 1, 1], sizes, vals)
+def get_val_sum(tmp_list):
+    sum_size = 0
+    sum_val = 0
+    for i, (size, value) in enumerate(tmp_list):
+        sum_val += value
+        sum_size += size
+    return sum_val if sum_size <= CAPACITY else 0
 
 
-print(res)
+def print_final_set(vector):
+    for idx, val in enumerate(vector):
+        if val == 1:
+            print("#", idx, "\t", sizes[idx], vals[idx])
 
+
+char_vector = get_char_vector(len(sizes))
+print(char_vector)
+# calc_list_vector_price(char_vector)
+
+# calc prices
+list_values = list()
+for idx, val in enumerate(char_vector):
+    list_values.append(calc_vector_price(val, sizes, vals))
+print(list_values)
+
+# find index of max
+max_idx = list_values.index(max(list_values))
+print("max_idx = ", max_idx)
+print_final_set(char_vector[max_idx])
